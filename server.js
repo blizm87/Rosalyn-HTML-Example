1a5c614ff21cd9ffdc183e0815a158dd760dde90
Justin Samuels <blizm87@gmail.com>
2017-07-08 10:45:35 -0700

    added dotenv module and added process.env to port

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -7,9 +8,10 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log('Server is listening!!!');
